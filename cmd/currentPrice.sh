c9561b6611550e2219156f84615ee998a44e3093
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-08-27 23:30:05 +0300

    ...

--- cmd/currentPrice.sh
+++ cmd/currentPrice.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.05-small -p nix jq
+#! nix-shell --pure -i dash -I channel:nixos-23.05-small -p cacert curl gnused
 set -eu
 
-./current.sh | jq '.centsPerKWh'
+curl -s https://spot.lahteenmaki.net/current.csv | sed 's/.*,//g'
