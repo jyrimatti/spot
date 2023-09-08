133d8addc2ef236440dff89af249089171f19fb7
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-09-08 22:05:21 +0300

    ...

--- cmd/currentPrice.sh
+++ cmd/currentPrice.sh
@@ -2,4 +2,4 @@
 #! nix-shell --pure -i dash -I channel:nixos-23.05-small -p cacert curl gnused
 set -eu
 
-curl -s https://spot.lahteenmaki.net/current.csv | sed 's/.*,//g'
+printf %.1f "$(curl -s https://spot.lahteenmaki.net/current.csv | sed 's/.*,//g')"
