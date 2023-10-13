e5b46bfd4b0a6c4029e33f9f4b4e63fd2be8df11
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-10-13 20:00:26 +0300

    deps

--- current.sh
+++ current.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i dash -I channel:nixos-23.05-small -p sqlite getoptions bc
+#! nix-shell -i dash -I channel:nixos-23.05-small -p sqlite getoptions bc coreutils
 set -eu
 
 TZ='Europe/Helsinki'
