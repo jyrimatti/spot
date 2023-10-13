0f30f6b3e9db03a0e3d4904beef2264bcd590858
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-10-13 20:00:49 +0300

    add delta hours for current price endpoint

--- current.sh
+++ current.sh
@@ -4,26 +4,29 @@ set -eu
 
 TZ='Europe/Helsinki'
 TAX=0
+DELTA=0
 
 parser() {
   msg -- "Returns the current spot price" ''
   setup   REST error:usage help:usage -- "Usage:  $0 [options]..." ''
   msg -- 'Options:'
-  param   TZ     -t --tz  init:=$TZ                      -- "Output time zone (default: $TZ)"
-  param   TAX    -x --tax init:=$TAX validate:percentage -- "Included tax percent (0-100) (default: $TAX)"
+  param   TZ     -t --tz    init:=$TZ                      -- "Output time zone (default: $TZ)"
+  param   TAX    -x --tax   init:=$TAX validate:percentage -- "Included tax percent (0-100) (default: $TAX)"
+  param   DELTA  -d --delta init:=$DELTA validate:integer  -- "Delta hours from current time, + for future, - for past (default: $DELTA)"
   disp    :usage -h --help
 }
 percentage() {
   case $OPTARG in ''|*[!0-9]*) return 1 ;; esac
   if [ "$OPTARG" -lt 0 ] || [ "$OPTARG" -gt 100 ] ; then return 1; fi
 }
+integer() { case "${OPTARG#[+-]}" in ''|*[!0-9]*) return 1 ;; esac; }
 
 eval "$(getoptions parser) exit 1"
 
 if [ $# != 0 ]; then usage; exit 1; fi
 
 export TZ
-now=$(date +%s)
+now=$(date +%s -d "now +$DELTA hours")
 offset=$(date +%:z)
 tax=$(echo "1 + $TAX/100" | bc -l)
 
