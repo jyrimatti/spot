d399610c585de97bc257e76f1916e4f93444f599
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-05-31 15:53:55 +0300

    Revert "--keep LD_LIBRARY_PATH"
    This reverts commit 8bb3e0635f53e2879e5bd10bf344e93ae62b4c9b.

--- spot_collect2db.sh
+++ spot_collect2db.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep LD_LIBRARY_PATH -I channel:nixos-23.11-small -i dash -p dash
+#! nix-shell --pure -I channel:nixos-23.11-small -i dash -p dash
 set -eu
 
 . ./spot_env.sh
