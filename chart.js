dd6b33457c8b71323f7bcdeea7fecb12c4ae8ffb
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-08-30 18:49:25 +0300

    adjust zoom

--- chart.js
+++ chart.js
@@ -339,7 +339,7 @@ let initSpotSeries = (dateFns, xAxis, series) => {
     0.3 + Math.abs(2*target.dataItem.dataContext.centsPerKWh / 40));
   
   series.events.once("datavalidated", ev =>
-    xAxis.zoomToDates(dateFns.addHours(new Date(), -18),
+    xAxis.zoomToDates(dateFns.addHours(new Date(), -16),
                       new Date(Math.max(...ev.target.data.values.map(x => x.instant)))));
     series.chart.set('visible', true);
 };
