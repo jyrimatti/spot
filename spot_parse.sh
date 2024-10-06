5d42bb20c879d3fd3a8e90874b1172f0cd9720f1
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-10-06 09:57:02 +0300

    apparently need to be overly explicit about xml lists...

--- spot_parse.sh
+++ spot_parse.sh
@@ -2,4 +2,4 @@
 #! nix-shell --pure -I channel:nixos-24.05-small -i dash -p jq yq gnugrep
 set -eu
 
-xq 'select(.Publication_MarketDocument) | .Publication_MarketDocument.TimeSeries[].Period | .timeInterval.start as $START | .Point | map({instant:$START, deltaHours:(.position | tonumber | .-1), centsPerKWh: (."price.amount" | tonumber | .*100/1000)})' | jq -s add | grep -v 'null'
+xq --xml-force-list TimeSeries 'select(.Publication_MarketDocument) | .Publication_MarketDocument.TimeSeries[].Period | .timeInterval.start as $START | .Point | map({instant:$START, deltaHours:(.position | tonumber | .-1), centsPerKWh: (."price.amount" | tonumber | .*100/1000)})' | jq -s add | grep -v 'null'
