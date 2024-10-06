69f686542c227ebc91cd28fc3e63b7d7e40a9087
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-10-06 10:08:51 +0300

    another XML array like

--- spot_parse.sh
+++ spot_parse.sh
@@ -2,4 +2,4 @@
 #! nix-shell --pure -I channel:nixos-24.05-small -i dash -p dash jq yq gnugrep
 set -eu
 
-xq --xml-force-list TimeSeries 'select(.Publication_MarketDocument) | .Publication_MarketDocument.TimeSeries[].Period | .timeInterval.start as $START | .Point | map({instant:$START, deltaHours:(.position | tonumber | .-1), centsPerKWh: (."price.amount" | tonumber | .*100/1000)})' | jq -s add | grep -v 'null'
+xq --xml-force-list TimeSeries --xml-force-list Period 'select(.Publication_MarketDocument) | .Publication_MarketDocument.TimeSeries[].Period[] | .timeInterval.start as $START | .Point | map({instant:$START, deltaHours:(.position | tonumber | .-1), centsPerKWh: (."price.amount" | tonumber | .*100/1000)})' | jq -s add | grep -v 'null'
